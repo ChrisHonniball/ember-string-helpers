7ac1b16326d8fd3d7c8e142da7dd171f51595da6
Chris Honniball <chonniball@gozooga.com>
2015-06-25 15:13:39 -0400

    merged file

--- index.js
+++ index.js
@@ -2,14 +2,5 @@
 'use strict';
 
 module.exports = {
-<<<<<<< Updated upstream
   name: 'ember-string-helper'
-=======
-  name: 'ember-string-helpers',
-  
-  included: function(app) {
-    this._super.included(app);
-    //app.import(app.bowerDirectory + '/moment/moment.js');
-  }
->>>>>>> Stashed changes
 };
