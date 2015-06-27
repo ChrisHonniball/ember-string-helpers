2d7c71800e57bb7f977842c375b3240dc710e753
Chris Honniball <chonniball@gozooga.com>
2015-06-27 00:08:00 -0400

    Updates

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -24,6 +24,9 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    'ember-highlight-code': {
+      style: 'tomorrow'
     }
   };
 
